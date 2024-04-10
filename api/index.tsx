953dcd819387758bd4f783e4c7f1edc0b637e48a
Muhamad Taopik <mr.94t3z@gmail.com>
2024-04-10 19:10:00 +0700

    ✨ adding load data from csv

--- api/index.tsx
+++ api/index.tsx
@@ -30,16 +30,86 @@ const stack = new StackClient({
   pointSystemId: parseInt(process.env.STACK_POINT_SYSTEM_ID || ''),
 });
 
+// Define the type for the CSV row
+interface TaskData {
+  taskType: string;
+  taskDescription: string;
+  pointsAwarded: number;
+  smartContract: string;
+  links: string;
+  claimAmount: number;
+  deadline: string;
+}
+
+const apiData: TaskData[] = [];
+
+// Define unsupported deadline format(s)
+const unsupportedDeadlineFormat = ['Unknown'];
+
+async function readCSVFromUrl(csvUrl: string) {
+  try {
+    const response = await fetch(csvUrl);
+
+    // Check if the fetch was successful
+    if (!response.ok) {
+      throw new Error(`Failed to fetch CSV: ${response.status} ${response.statusText}`);
+    }
+
+    const csvText = await response.text();
+    const rows = csvText.split('\n');
+
+    rows.forEach((row, index) => {
+      if (index === 0 || row.trim() === '') return; // Skip header row and empty rows
+
+      const columns = row.split(',');
+      if (columns.length < 7) { // Assuming 7 columns expected
+        console.error(`Skipping malformed row: ${row}`);
+        return;
+      }
+
+      const deadline = columns[6].trim();
+
+      if (!unsupportedDeadlineFormat.includes(deadline)) {
+        const taskData: TaskData = {
+          taskType: columns[0],
+          taskDescription: columns[1],
+          pointsAwarded: parseInt(columns[2]),
+          smartContract: columns[3],
+          links: columns[4],
+          claimAmount: parseInt(columns[5]),
+          deadline: deadline,
+        };
+        apiData.push(taskData);
+      }
+    });
+
+    console.log('CSV data successfully processed:');
+    console.log(apiData); // Log the loaded CSV data to the console
+  } catch (error) {
+    console.error('Error loading or processing CSV:', error);
+  }
+}
+
+
+// Call function to populate data, passing the CSV URL as an argument
+const csvUrl = 'https://raw.githubusercontent.com/Mr94t3z/highmiles-quest/master/api/resources/HighMiles%20Tasks%20-%20April%202024.csv';
+readCSVFromUrl(csvUrl);
+
 
 app.frame('/', async (c) => {
   const {buttonValue} = c;
   
   if (buttonValue === 'add') {
-    await stack.track("DailyWalletLogin", {
-      points: 15,
-      account: "0x2eeb301387D6BDa23E02fa0c7463507c68b597B5",
-      uniqueId: `${(new Date()).toISOString().split('T')[0]}-0x2eeb301387D6BDa23E02fa0c7463507c68b597B5`,
+    await stack.track("user_signup", {
+      points: -50,
+      account: "0x130946d8dF113e45f44e13575012D0cFF1E53e37"
     });
+    
+    // await stack.track("DailyWalletLogin", {
+    //   points: 15,
+    //   account: "0x2eeb301387D6BDa23E02fa0c7463507c68b597B5",
+    //   uniqueId: `${(new Date()).toISOString().split('T')[0]}-0x2eeb301387D6BDa23E02fa0c7463507c68b597B5`,
+    // });
   }
 
   return c.res({
