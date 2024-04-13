c1884bef09f80fc5385f392fb74e37c6da7b29af
Muhamad Taopik <mr.94t3z@gmail.com>
2024-04-13 23:52:22 +0700

    ✅ quest: 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14 finished

--- api/index.tsx
+++ api/index.tsx
@@ -1,6 +1,8 @@
 import { Button, Frog } from 'frog'
 import { handle } from 'frog/vercel'
 import { StackClient } from "@stackso/js-core";
+import { Alchemy, Network } from "alchemy-sdk";
+import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 
 // Uncomment this packages to tested on local server
@@ -42,6 +44,14 @@ const stack = new StackClient({
   apiKey: process.env.STACK_API_KEY || '', 
   pointSystemId: parseInt(process.env.STACK_POINT_SYSTEM_ID || ''),
 });
+
+// Initialize Alchemy client
+const config = {
+  apiKey: process.env.ALCHEMY_API_KEY || 'demo',
+  network: Network.BASE_MAINNET,
+};
+
+const alchemy = new Alchemy(config);
 
 // Get the current date
 const currentDate = new Date();
@@ -1269,21 +1279,98 @@ app.frame('/11th-quest', async (c) => {
     const data = await response.json();
     const userData = data.users[0];
 
-    // // User connected wallet address
-    // const eth_addresses = userData.verified_addresses.eth_addresses.toString().toLowerCase();
+    // User connected wallet address
+    const eth_addresses = userData.verified_addresses.eth_addresses.toString().toLowerCase();
 
+    const targetAddress = process.env.COINBASE_COMMERCE_WALLET_ADDRESS || '';
 
-    // stack.track("Buy 1 - 747 Gear from Warpshop Frames", {
-    //   points: 747,
-    //   account: eth_addresses,
-    //   uniqueId: eth_addresses
-    // });
+    // Function to extract wallet addresses from all topics in a log
+    function parseAddressesFromTopics(log: { topics: any[]; }) {
+      const addresses: string[] = [];
+      log.topics.forEach(topic => {
+        addresses.push(`0x${topic.slice(26)}`); // Slice the topic to get the address
+      });
+      return addresses;
+    }
 
-    // stack.track("Buy 2 - 747 Gear from Warpshop Frames", {
-    //   points: 747,
-    //   account: eth_addresses,
-    //   uniqueId: eth_addresses
-    // });
+    let qualifiedTransactions: any[] = [];
+
+    // Function to fetch transaction data from Tatum API
+    async function getTransactionData(hash: string) {
+      try {
+        const resp = await fetch(
+          `https://api.tatum.io/v3/base/transaction/${hash}`,
+          {
+            method: 'GET',
+            headers: {
+              'x-api-key': process.env.TATUM_API_KEY || '',
+            }
+          }
+        );
+    
+        if (resp.ok) {
+          const data = await resp.json(); // Parse response as JSON
+          const logs = data.logs || []; // Extract logs field, or default to an empty array if logs are not available
+    
+          // Parse all logs, filter, and extract addresses from all topics for logs that match the target address
+          const matchingLogs = logs.filter((log: { topics: any[]; }) => parseAddressesFromTopics(log).includes(targetAddress));
+          
+          if (matchingLogs.length > 0) {
+            return { hash, qualified: true };
+          } else {
+            return { hash, qualified: false };
+          }
+        } else {
+          console.error(`Failed to fetch transaction data for hash ${hash}:`, resp.statusText);
+          return { hash, qualified: false };
+        }
+      } catch (error) {
+        console.error(`Error fetching transaction data for hash ${hash}:`, error);
+        return { hash, qualified: false };
+      }
+    }
+
+    // Function to get asset transfers from Alchemy API and process them
+    async function getAssetTransfers() {
+      try {
+        const res = await alchemy.core.getAssetTransfers({
+          fromAddress: eth_addresses,
+          toAddress: process.env.COINBASE_COMMERCE_SMART_CONTRACT_ADDRESS,
+          excludeZeroValue: true,
+          category: ["external"] as any,
+        });
+    
+        const hashes = res.transfers.map(transfer => transfer.hash);
+    
+        for (const hash of hashes) {
+          const transactionData = await getTransactionData(hash);
+          if (transactionData.qualified) {
+            qualifiedTransactions.push(transactionData);
+            if (qualifiedTransactions.length >= 5) {
+              break; // Exit loop if 5 qualified transactions are found
+            }
+          }
+        }
+    
+        if (qualifiedTransactions.length > 0) {
+          for (let i = 1; i <= qualifiedTransactions.length; i++) {
+            await stack.track(`Buy ${i} - 747 Gear from Warpshop Frames`, {
+              points: 747,
+              account: eth_addresses,
+              uniqueId: eth_addresses
+            });
+            console.log(`User qualified for transaction ${i}!`);
+          }
+        } else {
+          console.log('User not qualified.');
+        }
+      } catch (error) {
+        console.error("Error getting asset transfers:", error);
+      }
+    }
+
+    // Call the function to get asset transfers and process them
+    await getAssetTransfers();
 
     return c.res({
       image: (
@@ -1326,15 +1413,15 @@ app.frame('/11th-quest', async (c) => {
           </div>
           <p style={{ fontSize: 30 }}>Task 11 - 747 Points 🎖️</p>
           <p style={{ margin : 0 }}>[ Buy - 747 Gear from Warpshop Frames ]</p>
-          {/* {castData && castData.some(data => data.result.casts.length > 0) ? (
+          {qualifiedTransactions && qualifiedTransactions.length > 0 ? (
             <p style={{ fontSize: 24 }}>Completed ✅</p>
           ) : (
             <p style={{ fontSize: 24 }}>Not qualified ❌</p>
-          )} */}
+          )}
         </div>
       ),
       intents: [
-        <Button.Link href='https://github.com/Mr94t3z/storage-farcaster-gift/tree/master?tab=readme-ov-file'>Buy ⌁</Button.Link>,
+        <Button.Link href='https://warpcast.com/boeing747/0x4ea6e3ba'>Buy ⌁</Button.Link>,
         <Button action='/11th-quest'>🔄 Refresh</Button>,
         <Button action='/10th-quest'>⏪ Back</Button>,
         <Button action='/12th-quest'>⏩️ Next</Button>,
